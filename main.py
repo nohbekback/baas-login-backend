09e27e058c5dc9a4fe318c0f7b82a0313cced6c6
Romaniello Roxana <romaroxana876@gmail.com>
2024-03-15 22:43:22 -0300

    subo api login

--- main.py
+++ main.py
@@ -23,9 +23,3 @@ def user_login(login: Login):
     else:
         raise HTTPException(status_code=401, detail="Credenciales incorrectas") 
 
-""" @app.post("/login")
-def user_login(login: Login):
-    if login.username not in login_db or login_db[login.username].password != login.password:
-        raise HTTPException(status_code=401, detail="Invalid credentials")
-    
-    return {"message": "Login successful"} """
